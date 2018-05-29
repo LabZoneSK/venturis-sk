7e8844ec74b1596993cf09cbe138fa98e7ce88d7
Martin Starosta <martin.starosta83@gmail.com>
2018-05-29 15:08:27 +0200

    Change hero gradient and add form submission

---
+++ src/scripts/venturis.js
@@ -0,0 +1,40 @@
+var init = function() {
+
+  var actionButton = document.getElementById('action-button');
+  var contactModal = document.getElementById('contact-form-modal');
+
+  var contactFormSubmitBtn = document.getElementById('contact-form-submit-btn');
+  var contactModalCloseBtn = document.getElementById('contact-form-modal-close');
+  var contactModalOutside = document.querySelector(".modal-background");
+
+
+  var closeModal = function (event) {
+    contactModal.classList.remove('is-active');
+  };
+
+  var handleFormSubmit = function (event) {
+    event.preventDefault();
+
+    var content = document.getElementById('form-content');
+    var thankYou = document.getElementById('form-thank-you');
+
+    content.classList.add('is-hidden');
+    thankYou.classList.remove('is-hidden');
+
+    setTimeout(function () {
+      var form = document.getElementById('contact-form');
+      form.submit();
+    }, 1000);
+  }
+
+  actionButton.addEventListener('click', function () {
+    contactModal.classList.add('is-active');
+  });
+
+  contactModalCloseBtn.addEventListener('click', closeModal);
+  contactModalOutside.addEventListener('click', closeModal);
+  contactFormSubmitBtn.addEventListener('click', handleFormSubmit);
+}
+
+init();
+

a9cde4d35ac6f65702e624c66153ea81e4b1d600
Martin Starosta <martin.starosta83@gmail.com>
2018-05-29 23:05:34 +0200

    Add minification for CSS and JS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,73 @@
-const autoprefixer = require('gulp-autoprefixer');
-const cache = require('gulp-cache');
-const clean = require('gulp-clean');
-const cssnano = require('gulp-cssnano');
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const imagemin = require('gulp-imagemin');
-const runSequence = require('run-sequence');
-const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer')
+const cache = require('gulp-cache')
+const clean = require('gulp-clean')
+const cssnano = require('gulp-cssnano')
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const imagemin = require('gulp-imagemin')
+const runSequence = require('run-sequence')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
 
 /* Clean BUILD folder */
 gulp.task('clean', () => {
-  return gulp.src('./build', {
+  return gulp
+    .src('./build', {
       read: false
     })
-    .pipe(clean());
-});
+    .pipe(clean())
+})
 
 /* Copy static (HTML, i18n, config.xml) files to BUILD folder */
 gulp.task('copy', () => {
-  gulp.src(['./src/*.html', './src/i18n/*.*'])
-  .pipe(gulp.dest('./build'))
-});
+  gulp.src(['./src/*.html', './src/i18n/*.*']).pipe(gulp.dest('./build'))
+})
 
 /* Process images */
 gulp.task('images', function () {
-  return gulp.src('./src/images/**/*.+(png|jpg|gif|svg)')
+  return gulp
+    .src('./src/images/**/*.+(png|jpg|gif|svg)')
     .pipe(cache(imagemin()))
     .pipe(gulp.dest('build/images'))
-});
+})
 
 /* Process scripts - JavaScript files */
 gulp.task('scripts', () => {
-  gulp.src('./src/scripts/*.js')
+  gulp
+    .src('./src/scripts/*.js')
+    .pipe(uglify())
     .pipe(gulp.dest('./build/scripts'))
-});
+})
 
 /* Process styles - SASS(CSS) files */
 gulp.task('styles', () => {
-  gulp.src('./src/sass/*.scss')
-    .pipe(sass({
-      style: 'expanded'
-    }))
-      .on('error', gutil.log)
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    /*.pipe(cssnano())*/
-    .pipe(gulp.dest('./build/css'));
-});
-
+  gulp
+    .src('./src/sass/*.scss')
+    .pipe(
+      sass({
+        style: 'expanded'
+      })
+    )
+    .on('error', gutil.log)
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/css'))
+})
 
 /* Build widget code */
-gulp.task('build', (cb) => {
-  runSequence(
-    'clean', ['copy', 'scripts', 'styles', 'images'],
-    cb
-  )
-});
+gulp.task('build', cb => {
+  runSequence('clean', ['copy', 'scripts', 'styles', 'images'], cb)
+})
 
 /* Watch for changes during development */
 gulp.task('watch-dev', () => {
-  gulp.watch('./src/**', ['styles', 'copy', 'scripts', 'images']);
+  gulp.watch('./src/**', ['styles', 'copy', 'scripts', 'images'])
 })
